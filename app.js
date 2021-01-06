54e51f7855a34e80a14f12abeb25a4e7a5b97fc5
alvarosopena <alvarosopenaf@gmail.com>
2021-01-05 23:21:46 -0300

    modificando html y css

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
-
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
